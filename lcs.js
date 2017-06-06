433dd9cec8ebb7012a4b53fa7435e03395dd0a10
terence <terence__zhang@163.com>
2017-06-07 01:29:53 +0800

    LCS

---
+++ lcs.js
@@ -0,0 +1,47 @@
+const lcs = (foo, bar) => {
+
+    const generate2DArray = (x, y) => {
+        let lenOfX = x.length, lenOfY = y.length
+        let arr = []
+
+        for(let i=0; i<lenOfX; i++){
+            arr[i] = []
+            for(let j=0; j<lenOfY; j++){
+                arr[i][j] = 0
+            }
+        }
+
+        for(let i=1; i<lenOfX; i++){
+            for(let j=1; j<lenOfY; j++){
+                arr[i][j] =
+                    x.charAt(i-1) === y.charAt(j-1)
+                        ? (arr[i-1][j-1] + 1)
+                        : Math.max(arr[i-1][j], arr[i][j-1])
+            }
+        }
+
+        return arr
+    }
+
+    const logLcs = (arr, x, y) => {
+        let tempArr = []
+        let i = x.length-1, j = y.length-1;
+
+        while(i>=0 && j>=0){
+            x.charAt(i) === y.charAt(j)
+                ? (tempArr.push(x.charAt(i)) && i-- && j--)
+                : (arr[i-1][j] > arr[i][j-1] ? i-- : j--)
+        }
+
+        return tempArr.reverse().join(',')
+    }
+
+    let arr = generate2DArray(foo, bar);
+    return logLcs(arr, foo, bar);
+}
+
+
+console.log(lcs('12345', '1245'))
+// 1,2,4,5
+
+
